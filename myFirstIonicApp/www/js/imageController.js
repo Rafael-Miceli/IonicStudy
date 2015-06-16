27e828a3d62dad95f4d1ac5695527584fb9ef386
rafael-miceli <rafael.miceli@hotmail.com>
2015-06-16 13:55:49 -0300

    Creating table with horizontal scroll

--- myFirstIonicApp/www/js/imageController.js
+++ myFirstIonicApp/www/js/imageController.js
@@ -5,12 +5,74 @@
 
     function imageController($scope) {
         
-        $scope.images = [];
- 
-        $scope.loadImages = function() {
-            for(var i = 0; i < 100; i++) {
-                $scope.images.push({id: i, src: "http://placehold.it/50x50"});
+        $scope.lockers = [];      
+        $scope.tableLockers = [];
+        
+        var locker = function(){
+            var id = id,
+            row = row,
+            col = col,
+            nome = nome;
+            
+            
+            return {
+                id: id,
+                row: row,
+                col: col,
+                nome: nome
             }
+        }
+        
+        $scope.loadLockers = function() {
+            var id = 1;
+            
+            for(var row = 1; row < 4; row++) {
+                
+                for (var col = 1; col < 13; col++) {
+                    var newLocker = new locker();
+                    
+                    newLocker.id = id;    
+                    newLocker.col = col;
+                    newLocker.row = row;
+                    newLocker.nome = "Row: " + row + "Col: " + col;                    
+                
+                    $scope.lockers.push(newLocker);
+                    
+                    id++;    
+                }                                
+                
+            }
+            
+            var groupArray = function groupBy( array , f )
+            {
+              console.log(array);                  
+              var groups = {};
+              array.forEach( function( o )
+              {
+                var group = JSON.stringify( f(o) );
+                groups[group] = groups[group] || [];
+                groups[group].push( o );  
+              });
+              return Object.keys(groups).map( function( group )
+              {
+                return groups[group]; 
+              })
+            }
+            
+            console.log($scope.lockers);
+            
+            var result = groupArray($scope.lockers, function(item)
+            {
+              return [item.row];
+            });
+            
+            $scope.tableLockers = result;
+            console.log(result);
+        }     
+        
+        
+        $scope.alugandoLocker = function(lockerId){
+            console.log(lockerId);
         }
         
     }
